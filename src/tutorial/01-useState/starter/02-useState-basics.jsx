ba507e8ddabe20f299918be7376d4ccd415f3977
Anthony Spedaliere <avspedaliere@gmail.com>
2023-04-14 14:00:14 -0700

    completed section 2 of section 01-useState

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,17 @@
+import { useState } from 'react';
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
-};
+
+  const [count,setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button type='button' className='btn' onClick={() => setCount(count + 1)}>Increase</button>
+    </div>  
+  );
+
+ 
+}
 
 export default UseStateBasics;
