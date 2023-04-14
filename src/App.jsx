95f2ac1830d98d4419b36f63e1eece9ea92b4716
Anthony Spedaliere <avspedaliere@gmail.com>
2023-04-13 17:48:41 -0700

    added tutorial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Our first vite project</h1>
-  )
+    <div className='container'>
+      <h2>Advanced React</h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
