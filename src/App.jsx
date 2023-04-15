06679c40f9ffa87363e4148bd2936e330d7ee6f4
Anthony Spedaliere <avspedaliere@gmail.com>
2023-04-14 18:27:15 -0700

    completed challenge for 03-useState-array in section 01-useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Starter from './tutorial/01-useState/starter/03-useState-array';
+import { data } from './data';
 
 function App() {
   return (
     <div className='container'>
-      <Starter/>
+      <Starter />
     </div>
   );
 }
--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,34 @@
+import { useState } from 'react';
+import { data } from '../../../data';
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+
+  const [people, setPeople] = useState(data);
+
+  const filterPeople = (id) => {
+    const filteredPeople = people.filter((person) => person.id !== id);
+    setPeople(filteredPeople);
+  }
+
+  const clearAllPeople = () => {    
+    setPeople([]);
+  }
+
+  return (
+    <div>
+      <ul>
+        {people.map((person) => {          
+          return(
+          <div key={person.id}>
+            <li>{person.name}</li>
+            <button type='button' onClick={() => filterPeople(person.id)} className='btn'>Delete</button>
+          </div>
+          )
+        })}
+      </ul>
+      <button style= {{marginTop: '2rem'}} type='button' onClick={() => clearAllPeople()} className='btn'>Clear All</button>
+    </div>    
+  )
 };
 
 export default UseStateArray;
