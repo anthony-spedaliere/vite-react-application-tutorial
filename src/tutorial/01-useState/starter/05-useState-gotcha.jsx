4adabaea0463cd69ed7839643bbeffaf6600231c
Anthony Spedaliere <avspedaliere@gmail.com>
2023-04-15 17:19:45 -0700

    finished 05-useState-gotcha challenge in section 01-useState

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+
+  // state values
+  const [value, setValue] = useState(0);
+
+  // component methods
+  const incrementValue = () => {
+    setTimeout(() => {
+      setValue((currentState) => {
+        return currentState + 1;
+      });
+    }, 3000);
+  };
+
+  //console.log(value);
+
+  return (
+    <div>
+      <h2>Value: {value}</h2>
+      <button type='button' className='btn' onClick={() => incrementValue()}>Increment</button>
+    </div>
+  )
 };
 
 export default UseStateGotcha;
