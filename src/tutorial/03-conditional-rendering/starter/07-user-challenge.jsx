cc174d834a3b779500fb10e2bcd36130305d2dfc
Anthony Spedaliere <avspedaliere@gmail.com>
2023-05-03 19:42:38 -0700

    finished user challenge in part 03-conidtional-rendering

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+
+  const [user, setUser] = useState(null);
+
+  function login() {
+    setUser('Frank');
+  }
+
+  function logout() {
+    setUser(null);
+  }
+
+  return (
+    <div>
+      {user ? <h4>Hello there, {user}</h4> : <h4>Please login</h4>}
+      <button className="btn" onClick={user ? logout : login}>{user ? 'Logout' : 'Login'}</button>
+    </div>
+  )
 };
 
 export default UserChallenge;
